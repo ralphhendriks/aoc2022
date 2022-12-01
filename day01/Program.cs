8e2404ed4c9265e077589941b4972dbb6fd71bf5
Ralph Hendriks <ralph.hendriks@greenflux.com>
2022-12-01 22:17:31 +0100

    Day 1 completed

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt");
+
+List<int> totals = new();
+int current = 0;
+foreach (var line in lines)
+{
+    if (line == string.Empty)
+    {
+        totals.Add(current);
+        current = 0;
+    }
+    else
+    {
+        current += int.Parse(line);
+    }
+}
+totals.Add(current);
+
+var answer1 = totals.Max();
+Console.WriteLine($"Answer part 1: {answer1}");
+
+var answer2 = totals.OrderByDescending(p => p).Take(3).Sum();
+Console.WriteLine($"Answer part 2: {answer2}");
