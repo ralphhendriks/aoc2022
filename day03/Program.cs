8831e8a91e683eaa6b776b4e617cbfa411976263
Ralph Hendriks <ralph.hendriks@greenflux.com>
2022-12-03 10:34:03 +0100

    Simplify day 3

--- day03/Program.cs
+++ day03/Program.cs
@@ -1,21 +1,7 @@
 ﻿var lines = File.ReadAllLines("input.txt");
 
-static IEnumerable<char> ItemTypesInBothCompartments(string rucksack) {
-    var s1 = new HashSet<char>(rucksack.Substring(0, rucksack.Length/2).ToCharArray());
-    var s2 = new HashSet<char>(rucksack.Substring(rucksack.Length/2).ToCharArray());
-    s2.IntersectWith(s1);
-    return s2;
-}
-
-static int Priority(char itemType) =>
-    (int)itemType switch {
-        var p when 65 < p && p <= 90 => p - 38,
-        var p when 97 < p && p <= 122 => p - 96,
-        _ => throw new ArgumentException("Unknown item type")
-    };
-
-var answer1 = lines.SelectMany(ItemTypesInBothCompartments).Select(Priority).Sum();
-Console.WriteLine($"Answer part 1: {answer1}");
+static IEnumerable<string> Compartmentize(string rucksack) =>
+    new string[] {rucksack.Substring(0, rucksack.Length/2), rucksack.Substring(rucksack.Length/2)};
 
 static IEnumerable<char> CommonItems(IEnumerable<string> groups)
 {
@@ -26,6 +12,16 @@ static IEnumerable<char> CommonItems(IEnumerable<string> groups)
     }
     return s;
 }
+
+static int Priority(char itemType) =>
+    (int)itemType switch {
+        var p when 65 < p && p <= 90 => p - 38,
+        var p when 97 < p && p <= 122 => p - 96,
+        _ => throw new ArgumentException("Unknown item type")
+    };
+
+var answer1 = lines.Select(Compartmentize).SelectMany(CommonItems).Select(Priority).Sum();
+Console.WriteLine($"Answer part 1: {answer1}");
 
 var answer2 = lines.Chunk(3).SelectMany(CommonItems).Select(Priority).Sum();
 Console.WriteLine($"Answer part 2: {answer2}");
