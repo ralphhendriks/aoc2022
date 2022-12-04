aac6ed1b542b9504fcac9e5e212bf3388128d276
Ralph Hendriks <ralph.hendriks@greenflux.com>
2022-12-04 09:04:59 +0100

    Day 4

---
+++ day04/Program.cs
@@ -0,0 +1,17 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+static int[] ParseInput(string l) =>
+    l.Split(new char[]{',','-'}).Select(int.Parse).ToArray();
+
+static bool FullyContains(int[] s) =>
+    (s[2] >= s[0] && s[3] <= s[1]) ||
+    (s[0] >= s[2] && s[1] <= s[3]);
+
+static bool Overlaps(int[] s) =>
+    Enumerable.Range(s[0], s[1]-s[0]+1).Intersect(Enumerable.Range(s[2], s[3]-s[2]+1)).Any();
+
+var answer1 = lines.Select(ParseInput).Where(FullyContains).Count();
+Console.WriteLine($"Answer part 1: {answer1}");
+
+var answer2 = lines.Select(ParseInput).Where(Overlaps).Count();
+Console.WriteLine($"Answer part 2: {answer2}");
