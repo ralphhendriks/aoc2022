3e402bfb8ccd4a4f5c45a73e48496f4c13b08d4a
Ralph Hendriks <ralph.hendriks@greenflux.com>
2022-12-06 07:53:21 +0100

    Day 6

--- day06/Program.cs
+++ day06/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllText("input.txt");
+
+static int FindDistinctCharacters(string datastream, int n)
+{
+    HashSet<char> buffer = new();
+    for (var i = n - 1; i < datastream.Length; i++)
+    {
+        buffer.Clear();
+        for (var j = i - n + 1; j <= i; j++) buffer.Add(datastream[j]);
+        if (buffer.Count == n) return i + 1;
+    }
+    return -1;
+}
+
+var answer1 = FindDistinctCharacters(input, 4);
+Console.WriteLine($"Answer part 1: {answer1}");
+var answer2 = FindDistinctCharacters(input, 14);
+Console.WriteLine($"Answer part 2: {answer2}");
