33f206409168f8870112cd22aa5772e5d415e479
Ralph Hendriks <ralph.hendriks@greenflux.com>
2022-12-17 21:53:59 +0100

    Part 2 not working

--- day11/Program.cs
+++ day11/Program.cs
@@ -1,19 +1,16 @@
-﻿var lines = File.ReadAllText("input.txt");
+﻿var lines = File.ReadAllText("test.txt");
 
-var monkeys =
-    lines
-        .Split(Environment.NewLine + Environment.NewLine)
-        .Select(Monkey.Create)
-        .ToList();
-
-static void PlayRound(List<Monkey> monkeys)
+static void PlayRounds(List<Monkey> monkeys, int rounds)
 {
-    foreach (var monkey in monkeys)
+    for (int r = 0; r < rounds; r++)
     {
-        monkey.InspectItems();
-        foreach (var (i, worryLevel) in monkey.Outbox)
+        foreach (var monkey in monkeys)
         {
-            monkeys[i].AddItem(worryLevel);
+            monkey.InspectItems();
+            foreach (var (i, worryLevel) in monkey.Outbox)
+            {
+                monkeys[i].AddItem(worryLevel);
+            }
         }
     }
 }
@@ -26,38 +23,60 @@ static void PrintInventory(List<Monkey> monkeys)
     }
 }
 
-// System.Console.WriteLine("Start:");
-// PrintInventory(monkeys);
-// PlayRound(monkeys);
-// Console.WriteLine("After round 1:");
-// PrintInventory(monkeys);
-
-// Play 20 rounds
-for (int i = 0; i < 20; i++)
+static void PrintInspections(List<Monkey> monkeys)
 {
-    PlayRound(monkeys);
+    for (int i = 0; i < monkeys.Count; i++)
+    {
+        Console.WriteLine($"Monkey {i} inspected items {monkeys[i].ItemsInspected} times.");
+    }
 }
 
-PrintInventory(monkeys);
+static long MonkeyBusiness(List<Monkey> monkeys) =>
+    monkeys
+        .Select(m => m.ItemsInspected)
+        .OrderByDescending(x => x)
+        .Take(2)
+        .Aggregate((x, y) => x * y);
 
-var answer1 = monkeys
-    .Select(m => m.ItemsInspected)
-    .OrderByDescending(x => x)
-    .Take(2)
-    .Aggregate((x, y) => x * y);
+var monkeysPart1 =
+    lines
+        .Split(Environment.NewLine + Environment.NewLine)
+        .Select(Monkey.Create)
+        .ToList();
+PlayRounds(monkeysPart1, 20);
+var answer1 = MonkeyBusiness(monkeysPart1);
 Console.WriteLine($"Answer part 1: {answer1}");
+
+var monkeysPart2 =
+    lines
+        .Split(Environment.NewLine + Environment.NewLine)
+        .Select(Monkey.CreateWithoutRelief)
+        .ToList();
+PlayRounds(monkeysPart2, 1000);
+PrintInspections(monkeysPart2);
+// PlayRounds(monkeysPart2, 10_000);
+// var answer2 = MonkeyBusiness(monkeysPart2);
+// Console.WriteLine($"Answer part 2: {answer2}");
 
 public class Monkey
 {
     private readonly List<long> _items;
     private readonly char _operator;
-    private readonly int? _operand2;
-    private readonly int _divisor;
+    private readonly long? _operand2;
+    private readonly long _divisor;
     private readonly int _monkeyIfTrue;
     private readonly int _monkeyIfFalse;
+    private readonly long _relief;
     private List<(int, long)> _outbox;
 
-    private Monkey(IEnumerable<long> items, char op, int? operand2, int divisor, int monkeyIfTrue, int monkeyIfFalse)
+    private Monkey(
+        IEnumerable<long> items,
+        char op,
+        long? operand2,
+        long divisor,
+        int monkeyIfTrue,
+        int monkeyIfFalse,
+        long relief)
     {
         _items = new List<long>(items);
         _operator = op;
@@ -65,6 +84,7 @@ public class Monkey
         _divisor = divisor;
         _monkeyIfTrue = monkeyIfTrue;
         _monkeyIfFalse = monkeyIfFalse;
+        _relief = relief;
         _outbox = new();
     }
 
@@ -82,7 +102,7 @@ public class Monkey
 
     public IEnumerable<(int, long)> Outbox => _outbox;
 
-    public int ItemsInspected { get; private set; }
+    public long ItemsInspected { get; private set; }
 
     public void AddItem(long worryLevel) => _items.Add(worryLevel);
 
@@ -101,11 +121,15 @@ public class Monkey
             '+' => worryLevel + op2,
             '*' => worryLevel * op2,
             _ => throw new InvalidOperationException("Unknown operator")
-        } / 3;
+        } / _relief;
         return newWorryLevel % _divisor == 0 ? (_monkeyIfTrue, newWorryLevel) : (_monkeyIfFalse, newWorryLevel);
     }
 
-    public static Monkey Create(string input)
+    public static Monkey Create(string input) => CreateInt(input, 3);
+
+    public static Monkey CreateWithoutRelief(string input) => CreateInt(input, 1);
+
+    private static Monkey CreateInt(string input, int relief)
     {
         var lines = input.Split(Environment.NewLine);
         var items = lines[1][18..].Split(',').Select(long.Parse);
@@ -114,6 +138,6 @@ public class Monkey
         var divisor = int.Parse(lines[3][21..]);
         var monkeyIfTrue = int.Parse(lines[4][29..]);
         var monkeyIfFalse = int.Parse(lines[5][30..]);
-        return new Monkey(items, op, operand2, divisor, monkeyIfTrue, monkeyIfFalse);
+        return new Monkey(items, op, operand2, divisor, monkeyIfTrue, monkeyIfFalse, relief);
     }
 }
